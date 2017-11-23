42666fc35bf8b7b86a19ca61333d6492e11dabf5
esrendon <esrv71@hotmail.com>
2017-11-23 12:31:12 -0600

    suitable commit message

--- app/src/main/java/com/amazonaws/mobile/samples/mynotes/NoteListActivity.java
+++ app/src/main/java/com/amazonaws/mobile/samples/mynotes/NoteListActivity.java
@@ -41,6 +41,8 @@ import com.amazonaws.mobile.samples.mynotes.data.Note;
 import com.amazonaws.mobile.samples.mynotes.data.NoteViewHolder;
 import com.amazonaws.mobile.samples.mynotes.data.NotesContentContract;
 import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobileconnectors.pinpoint.analytics.AnalyticsClient;
+import com.amazonaws.mobileconnectors.pinpoint.analytics.AnalyticsEvent;
 
 /**
  * An activity representing a list of Notes. This activity
@@ -145,6 +147,15 @@ public class NoteListActivity
             public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
                 final NoteViewHolder noteHolder = (NoteViewHolder) viewHolder;
                 ((NotesAdapter) notesList.getAdapter()).remove(noteHolder);
+
+                // Send Custom Event to Amazon Pinpoint
+                final AnalyticsClient mgr = AWSProvider.getInstance()
+                        .getPinpointManager()
+                        .getAnalyticsClient();
+                final AnalyticsEvent evt = mgr.createEvent("DeleteNote")
+                        .withAttribute("noteId", noteHolder.getNote().getNoteId());
+                mgr.recordEvent(evt);
+                mgr.submitEvents();
             }
 
             @Override
